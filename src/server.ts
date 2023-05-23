b4c39353ab55e227ebef5b460846a5dca128314e
snowdenmoses <111445710+AbubakarMoses@users.noreply.github.com>
2023-05-24 00:39:59 +0100

    adds sql searching functionality

--- src/server.ts
+++ src/server.ts
@@ -15,12 +15,22 @@ app.get("/", (req,res)=>{
     // const from = req.query.from ? req.query.from : yearStart
     // const to = req.query.to ? req.query.to : today
     const page = req.query.page ? req.query.page : Number(1)
-    const fullname = req.query.search ? `%${req.query.search}%` : "%%add"
+    const fullname = req.query.search ? `%${String(req.query.search).toLowerCase()}%` : "%%add"
 
     const offset = Number(limit) * (Number(page) - 1)
 
     dbConnect
-    .query('SELECT * FROM "Staff" WHERE "FullName" LIKE $1 ORDER BY "Id" ASC LIMIT $2 OFFSET $3',[fullname,limit, offset])
+        .query(`SELECT * 
+        FROM "Staff" 
+        WHERE lower("FullName")
+        LIKE $1 
+            OR "Email" 
+        LIKE $1
+            OR lower("Gender")
+        LIKE $1
+            OR lower("Role") 
+        LIKE $1
+          ORDER BY "Id" ASC LIMIT $2 OFFSET $3`,[fullname,limit, offset])
     .then(data =>{
         res.json({ data, count: data.length })
     })
